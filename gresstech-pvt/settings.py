6f7b63146f52e8887cb567495c52cc2d39c5e43e
SRIKANTH <sreekanthmattupalli@gmail.com>
2019-04-26 08:49:34 -0400

    settings and wsgi is change

--- gresstech-pvt/settings.py
+++ gresstech-pvt/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Firstpython.urls'
+ROOT_URLCONF = 'gresstech-pvt.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Firstpython.wsgi.application'
+WSGI_APPLICATION = 'gresstech-pvt.wsgi.application'
 
 
 # Database
--- gresstech-pvt/wsgi.py
+++ gresstech-pvt/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Firstpython.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gresstech-pvt.settings')
 
 application = get_wsgi_application()
